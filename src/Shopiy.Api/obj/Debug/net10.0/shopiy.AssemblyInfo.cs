acd0c763595b3befb8679a539243f22ff9874440
JonathanGhaly <jghaly00@gmail.com>
2026-06-26 22:45:01 +0300

    chore: generate assembly information and build cache files

--- src/Shopiy.Api/obj/Debug/net10.0/shopiy.AssemblyInfo.cs
+++ src/Shopiy.Api/obj/Debug/net10.0/shopiy.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("shopiy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30aedea653e788c6a664037dfd4b158a89214ec4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80d609fe87c5c5ce6191f8e41188f82224a876f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("shopiy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("shopiy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
